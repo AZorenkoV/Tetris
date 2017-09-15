9bb9f8cd511d60a4ba128f3d1b8d46c2c2e92d02
zorenkoav <azorenko@gmail.com>
2017-09-15 17:13:08 +0300

    Make class Block

--- src/ua/raccoon/GameTetris.java
+++ src/ua/raccoon/GameTetris.java
@@ -37,7 +37,7 @@ public class GameTetris {
     private Random random = new Random();
     private Figure figure = new Figure();
     private boolean gameOver = false;
-    private final  int[][] GAME_OVER_MSG = {
+    private final int[][] GAME_OVER_MSG = {
             {0,1,1,0,0,0,1,1,0,0,0,1,0,1,0,0,0,1,1,0},
             {1,0,0,0,0,1,0,0,1,0,1,0,1,0,1,0,1,0,0,1},
             {1,0,1,1,0,1,1,1,1,0,1,0,1,0,1,0,1,1,1,1},
@@ -57,9 +57,117 @@ public class GameTetris {
     }
 
     private void go(){
+        frame = new JFrame(TITLE_OF_PROGAM);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(FIELD_WIDTH * BLOCK_SIZE + FIELD_DX, FIELD_HEIGHT * BLOCK_SIZE + FIELD_DY);
+        frame.setLocation(START_LOCATION, START_LOCATION);
+        frame.setResizable(false);
+        canvasPanel.setBackground(Color.BLACK);
+        frame.addKeyListener(new KeyAdapter() {
+                                 @Override
+                                 public void keyPressed(KeyEvent keyEvent) {
+                                     if(!gameOver) {
+                                         if(keyEvent.getKeyCode() == DOWN) figure.drop();
+                                         if(keyEvent.getKeyCode() == UP) figure.rotate();
+                                         if(keyEvent.getKeyCode() == LEFT || keyEvent.getKeyCode() == RIGHT)
+                                             figure.move(keyEvent.getKeyCode());
+                                     }
+                                     canvasPanel.repaint();
+                                 }
+                             });
+        frame.getContentPane().add(BorderLayout.CENTER, canvasPanel);
+        frame.setVisible(true);
+
+        Arrays.fill(mine[FIELD_HEIGHT], 1);
+
+        //main loop of game
+        while(!gameOver){
+            try{
+                Thread.sleep(SHOW_DELAY);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            canvasPanel.repaint();
+            if(figure.isTouchGround()){
+                figure.leaveOnTheGround();
+                checkFilling();
+                figure = new Figure();
+                gameOver = figure.isCrossGround();
+            } else {
+                figure.stepDown();
+            }
+        }
+    }
+
+    private void checkFilling(){
 
     }
 
     private class Figure {
+
+        public boolean isTouchGround(){
+            return false;
+        }
+
+        public void leaveOnTheGround(){
+
+        }
+
+        public void drop() {
+
+        }
+
+        public void rotate() {
+
+        }
+
+        public void move(int keyCode) {
+
+        }
+
+        public boolean isCrossGround() {
+            return false;
+        }
+
+        public void stepDown() {
+
+        }
+    }
+
+    private class Block {
+        private int x, y;
+
+        public Block(int x, int y){
+            setX(x);
+            setY(y);
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public int getX(){
+            return x;
+        }
+
+        public int getY(){
+            return y;
+        }
+
+        public void paint(Graphics g, int color) {
+            g.setColor(new Color(color));
+            g.drawRoundRect(x*BLOCK_SIZE+1, y*BLOCK_SIZE+1, BLOCK_SIZE-2, BLOCK_SIZE-2, ARC_RADIUS, ARC_RADIUS);
+        }
+    }
+
+    private class Canvas extends JTable {
+        @Override
+        public void paint(Graphics g){
+            super.paint(g);
+        }
     }
 }
